2ecd8acb73829be82fe14ca572d3178057e7c09c
Vicente Guijarro <lx05204@innovaccion.mx>
2023-01-08 13:25:53 -0600

    comentamos el codigo JS

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
-
+//declaramos la funcion que se realiza con el "onclick" del HTML.
 const fetchPokemon = async () => {
+    //aqui hacemos referencia a lo que buscara la petición.
     const pokeNameInput = document.getElementById("pokeSearch");
+    //aqui obtenemos el valor de la referencia anterior
     let pokeSearch = pokeNameInput.value;
+    //aqui convertimos a minusculas porque la API funciona solo con minusculas.
     pokeSearch = pokeSearch.toLowerCase();
+    //aqui creamos una constante la cual realiza la busqueda en la api con las referencias anteriores
     const url = `https://pokeapi.co/api/v2/pokemon/${pokeSearch}`;
+    //aqui realizamos la peticion 
     let data = await fetch(url).then((res) => {
+    //Aqui si la respuesta de la API es "200" osea que esta mal el valor indicado nos muestra una alerta y un picachu llorando,    
         if (res.status != "200") {
             console.log(res);
             pokeImage("./images/sad-pikachu.gif")
             alert("Intentalo de nuevo!")
         }
         else {
+    //si la respuesta es favorable nos regresa un JSON almacenado en "data"
             return res.json();
         }
     })
     if (data) {
         console.log(data);
+        //aqui partiendo de la respuesta creamos variables para almacenar la informacion que necesitamos mostrar.
         let pokeImg = data.sprites.other.dream_world.front_default;
         let pokeName = data.name;
         let idPokemon = data.id;
@@ -25,7 +33,7 @@ const fetchPokemon = async () => {
         let defensePokemon = info[2].base_stat;
         
         
-
+        //aqui llamamos a las variables que creamos mediante constantes.
         pokeImage(pokeImg);
         pokename(pokeName);
         pokeId(idPokemon);
@@ -34,7 +42,8 @@ const fetchPokemon = async () => {
         pokeDefense(defensePokemon);
     }
 }
-
+//Estas constantes que reaalizamos hacen referencia al tado que reciben de las variables antes creadas
+//y los valores que recibe se agreagan al HTML 
 const pokeImage = (url) => {
     const pokePhoto = document.getElementById("pokeImg");
     pokePhoto.src = url;
